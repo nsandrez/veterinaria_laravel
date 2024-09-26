9a71216f6a50d9b6d117975ee8764df837cf2585
nsandrez <n.orrego@outlook.cl>
2024-09-26 20:01:18 -0300

    agregando vista de errores

---
+++ resources/views/error/403.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tierra Animales - Acceso Denegado</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="bg-white flex items-center justify-center h-screen">
+    <div class="text-center">
+        <div class="inline-flex items-center justify-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16 text-primary" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor">
+                <circle cx="11" cy="4" r="2" />
+                <circle cx="18" cy="8" r="2" />
+                <circle cx="20" cy="16" r="2" />
+                <path
+                    d="M9 10a5 5 0 0 1 5 5v3.5a3.5 3.5 0 0 1-6.84 1.045Q6.52 17.48 4.46 16.84A3.5 3.5 0 0 1 5.5 10Z" />
+            </svg>
+        </div>
+        <h1 class="text-4xl font-bold text-gray-800 mt-4">Oops, acceso denegado!</h1>
+        <p class="mt-2 text-gray-500 max-w-md mx-auto">Lo sentimos, pero no tienes permiso para acceder a esta página.
+            Por favor consulte con su administrador o vuelve a intentarlo más tarde.</p>
+        <a href="/"
+            class="mt-6 inline-block px-6 py-3 bg-black text-white rounded-full hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-600">
+            Ir a la página de inicio
+        </a>
+    </div>
+</body>
+
+</html>
